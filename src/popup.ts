aa4ee478f038db95a4b705a0428076ee913c483a
Ben Mezger <me@benmezger.nl>
2020-04-09 23:38:53 -0300

    Feat: Add popup HTML which generates an email

--- src/popup.ts
+++ src/popup.ts
@@ -1,32 +1,22 @@
-import * as moment from 'moment';
-import * as $ from 'jquery';
+function splitEmail(email: string): string[]{
+  return email.split("@");
+}
 
-let count = 0;
-
-$(function() {
+document.addEventListener("DOMContentLoaded", function() {
   const queryInfo = {
     active: true,
     currentWindow: true
   };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    $('#url').text(tabs[0].url);
-    $('#time').text(moment().format('YYYY-MM-DD HH:mm:ss'));
-  });
-
-  chrome.browserAction.setBadgeText({text: count.toString()});
-  $('#countUp').click(()=>{
-    chrome.browserAction.setBadgeText({text: (++count).toString()});
-  });
-
-  $('#changeBackground').click(()=>{
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        color: '#555555'
-      },
-      function(msg) {
-        console.log("result message:", msg);
-      });
+  chrome.tabs.query(queryInfo, tabs => {
+    var url = new URL(tabs[0].url);
+    chrome.storage.sync.get(["email"], result => {
+      const userEmail = splitEmail(result.email).splice(1)
+      var emailElem = document.getElementById('email');
+      setTimeout(function () {
+          emailElem.textContent = url.host + "@" + userEmail.join("");
+      }, 750);
     });
   });
 });
+
