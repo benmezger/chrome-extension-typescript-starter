b9941c11a40b89f7f409e29cf46dd5a685b10e05
Ben Mezger <me@benmezger.nl>
2020-04-09 23:06:49 -0300

    Feat: Save email to Chrome's storage

--- src/options.ts
+++ src/options.ts
@@ -2,31 +2,27 @@ import * as $ from 'jquery';
 
 // Saves options to chrome.storage.sync.
 function save_options() {
-  var color = $('#color').val();
-  var likesColor = $('#like').prop('checked');
-  chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = $('#status');
-    status.text('Options saved.');
-    setTimeout(function() {
-      status.text('');
-    }, 750);
-  });
+    const email = $('#email').val();
+    chrome.storage.sync.set({
+        email: email
+    }, function(){
+      const status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+          status.textContent = '';
+      }, 750);
+    });
 }
+
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items: {favoriteColor, likesColor}) {
-    $('#color').val(items.favoriteColor);
-    $('#like').prop('checked', items.likesColor);
+    email: null,
+  }, function(items: {email}) {
+    $('#email').val(items.email);
   });
 }
 
